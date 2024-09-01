f22ae7aca3c74dffbc93bb4fb79d53822ecc8d27
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2024-09-01 18:24:23 +0500

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+    <div className="text-9xl text-green-900">
+      
+      HomePage
+      </div>
+    </>
+  )
+}
+
+export default HomePage
